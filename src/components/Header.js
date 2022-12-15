4232b42439110f52792aa2a20aae0445d571492b
KushagraGitter <kushagra305@gmail.com>
2022-12-15 18:50:51 +0530

    3 files modified

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,5 +3,10 @@ import Cart from './Cart';
 import SearchBox from './SearchBox';
 
 export default function Header({ children }) {
-  return <div className="app-header">{children}</div>;
+  return (
+    <div className="app-header">
+      <h1 className="app-header-title">Mini Shopping</h1>
+      {children}
+    </div>
+  );
 }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,27 +5,34 @@ import useFetch from '../hooks/useFetch';
 export default function ProductList({ products, handleAddToCart }) {
   return (
     <ul className="product-list">
-      {' '}
-      {products.map((product) => {
-        return (
-          <Link className="product-item" to={`/productDetail/${product.id}`}>
-            <li key={product.id}>
-              <div className="product-item-card">
-                <h2>{product.title}</h2>
-                <img src={product.image} height={300} width={300}></img>
-                <p>{product.description}</p>
-                <p>{product.price}</p>
-                <button
-                  className="product-item-addtocart"
-                  onClick={() => handleAddToCart(product.id)}
-                >
-                  Add To Cart
-                </button>
-              </div>
-            </li>
-          </Link>
-        );
-      })}
+      {products &&
+        products.map((product) => {
+          return (
+            <Link
+              className="product-item"
+              to={`/productDetail/${product.id}`}
+              key={product.id}
+            >
+              <li key={product.id}>
+                <div className="product-item-card">
+                  <h2>{product.title}</h2>
+                  <img src={product.image} height={300} width={300}></img>
+                  <p>{product.description}</p>
+                  <p>{product.price}</p>
+                  <button
+                    className="product-item-addtocart"
+                    onClick={(evt) => {
+                      evt.preventDefault();
+                      handleAddToCart(product.id);
+                    }}
+                  >
+                    Add To Cart
+                  </button>
+                </div>
+              </li>
+            </Link>
+          );
+        })}
     </ul>
   );
 }
